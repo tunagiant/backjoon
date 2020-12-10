a40615b471c1152319d947bc6e183a213067520c
tunagiant <68101999+tunagiant@users.noreply.github.com>
2020-12-10 19:25:04 +0900

    Create Main.java

---
+++ src/chap08_Math2/n1002/Main.java
@@ -0,0 +1,26 @@
+package chap08_Math2.n1002;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int T = 0;
+		int x1 = 0, y1 = 0, r1 = 0, x2 = 0, y2 = 0, r2 = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		T = scanner.nextInt();
+		
+		for (int i = 0; i < T; i++) {
+			x1 = scanner.nextInt();
+			y1 = scanner.nextInt();
+			r1 = scanner.nextInt();
+			x2 = scanner.nextInt();
+			y2 = scanner.nextInt();
+			r2 = scanner.nextInt();
+			
+		}
+	}
+
+}
