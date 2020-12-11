549c6327920aa3de2a173d682f1b3ab2311cd66e
tunagiant <68101999+tunagiant@users.noreply.github.com>
2020-12-12 01:16:53 +0900

    Create Main.java

---
+++ src/chap11_BruteForce/n1018/Main.java
@@ -0,0 +1,63 @@
+package chap11_BruteForce.n1018;
+
+import java.util.Scanner;
+
+public class Main {
+
+	static char[][] base;
+	static int MIN = 64;
+	static char[][] check = { "WBWBWBWB".toCharArray(), "BWBWBWBW".toCharArray() };
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+		int M = scanner.nextInt();
+
+		scanner.nextLine();
+
+		String str;
+		base = new char[N][M];
+
+		for (int i = 0; i < N; i++) {
+			str = scanner.nextLine().trim();
+
+			for (int j = 0; j < M; j++) {
+				base[i][j] = str.charAt(j);
+			}
+		}
+
+		for (int i = 0; i < N - 7; i++) {
+			for (int j = 0; j < M - 7; j++) {
+				find(i, j);
+			}
+		}
+
+		System.out.println(MIN);
+
+	}
+
+	private static int find(int x, int y) {
+
+		int count = 0;
+
+		for (int i = 0; i < 2; i++) {
+			count = 0;
+			for (int j = 0; j < 8; j++) { // N
+				for (int k = 0; k < 8; k++) { // M
+					if (base[x + j][y + k] != check[(i + j) % 2][k]) {
+						count++;
+
+					}
+				}
+			}
+			if (count <= MIN) {
+				MIN = count;
+			}
+		}
+		return MIN;
+
+	}
+
+}
